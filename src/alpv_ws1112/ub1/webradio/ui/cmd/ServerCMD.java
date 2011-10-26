6a49c3049c484611f50a9ecb660f0209d15a1b2c
christianrudolph <christian.rudolph@sap.com>
2011-10-26 13:28:58 +0200

    Signed-off-by: christianrudolph <christian.rudolph@sap.com>

---
+++ src/alpv_ws1112/ub1/webradio/ui/cmd/ServerCMD.java
@@ -0,0 +1,12 @@
+package alpv_ws1112.ub1.webradio.ui.cmd;
+
+import alpv_ws1112.ub1.webradio.ui.ServerUI;
+
+public class ServerCMD implements ServerUI {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+}
